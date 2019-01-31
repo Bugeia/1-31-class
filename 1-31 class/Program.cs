f8047a689a902049cee57079bfcf21fcec2518a5
bugei <yanjunyang@mail.usf.edu>
2019-01-31 17:59:35 -0500

    version 2

--- 1-31 class/Program.cs
+++ 1-31 class/Program.cs
@@ -6,7 +6,17 @@ namespace _1_31_class
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name.");
+            try
+            {
+                string input = Console.ReadLine();
+
+                Console.WriteLine(input);
+            }
+            catch
+            {
+                Console.WriteLine("Press any key to continue.");
+            }
         }
     }
 }
